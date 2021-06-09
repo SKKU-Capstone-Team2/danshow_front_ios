f9fcdb7d0b4e9564a798c3acdd71c1c13e0ae672
Lee Jeong Min <53992007+hustle-dev@users.noreply.github.com>
2021-06-09 18:56:34 +0900

    video module edit

--- component/tp_detail.js
+++ component/tp_detail.js
@@ -1,64 +1,63 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {SafeAreaView, StyleSheet, Text, View, ScrollView, Image, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Video from 'react-native-video';
 
-export default class Tp_detail extends React.Component{
+export default function Tp_detail({navigation}) {
 
-  render () {
-    return (
-      <SafeAreaView style={styles.container}>
-        <ScrollView>
-          <View style = {styles.topHeader}>
-              <Image 
-              style = {{margin: 15, marginRight:90}}
-              source={require("./icon/danshow_logo.png")}></Image>
-              <Icon name="funnel-outline" size={30} style={{padding:10}}></Icon>
-              <Icon name="search-outline" size={30}style={{padding:10}}></Icon>
-              <Icon name="notifications-outline" size={30}style={{padding:10}}></Icon>
+  const videoRef = useRef(null);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style = {styles.topHeader}>
+            <Image 
+            style = {{margin: 15, marginRight:90}}
+            source={require("./icon/danshow_logo.png")}></Image>
+            <Icon name="funnel-outline" size={30} style={{padding:10}}></Icon>
+            <Icon name="search-outline" size={30}style={{padding:10}}></Icon>
+            <Icon name="notifications-outline" size={30}style={{padding:10}}></Icon>
+        </View>
+        <Video
+        source={{ uri: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1' }}
+        style={{ width: 300, height: 300 }}
+        controls={true}
+        ref={videoRef}/>
+        {/* <View style={styles.resultImageadd}>
+          <Text style={{color:'white', fontWeight:'bold', margin:10}}>Result Image</Text>
+          <View style={{justifyContent:'center', alignItems:'center', marginTop:25}}>
+              <Icon color="white" size={30} name="add-circle-outline"></Icon>
           </View>
-          {/* <View style={styles.resultImageadd}>
-            <Text style={{color:'white', fontWeight:'bold', margin:10}}>Result Image</Text>
-            <View style={{justifyContent:'center', alignItems:'center', marginTop:25}}>
-                <Icon color="white" size={30} name="add-circle-outline"></Icon>
-            </View>
-          </View> */}
-          <Video
-          source={{ uri: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1'}}
-          style={{ width: 300, height: 300 }}
-          controls={true}
-          ref={(ref) => {
-          this.player = ref
-          }} />
-          <View style={{backgroundColor: '#E9E8E8', width:390, height:4, marginTop:20}}></View>
-          <View style={{marginLeft:20, marginRight:20, marginTop:20}}>
-            <Text style={{fontWeight:'bold', fontSize:15}}>Test Result</Text>
-            <View style={{flexDirection:'row', marginTop:20}}>
-              <Image 
-              style={{width:130, height:130}}
-              source={require("./icon/circle.png")}></Image>
-              <View style={{margin:20}}>
-                <Text style={{fontWeight:'bold', fontSize:30, marginBottom:10}}>Score: 95</Text>
-                <Text style={{fontWeight:'bold', fontSize:20, marginBottom:10}}>It looks like a dancer!</Text>
-                <Text style={{fontWeight:'bold', fontSize:25, marginBottom:10, color:'#1058F4', textAlign:'center'}}>135th place</Text>
-                <TouchableOpacity style={styles.touchOpa}>
-                  <Text style={{color:'white', fontWeight: 'bold', fontSize:12}}>Rechallenge?</Text>
-                </TouchableOpacity>
-              </View>
+        </View> */}
+        <View style={{backgroundColor: '#E9E8E8', width:390, height:4, marginTop:20}}></View>
+        <View style={{marginLeft:20, marginRight:20, marginTop:20}}>
+          <Text style={{fontWeight:'bold', fontSize:15}}>Test Result</Text>
+          <View style={{flexDirection:'row', marginTop:20}}>
+            <Image 
+            style={{width:130, height:130}}
+            source={require("./icon/circle.png")}></Image>
+            <View style={{margin:20}}>
+              <Text style={{fontWeight:'bold', fontSize:30, marginBottom:10}}>Score: 95</Text>
+              <Text style={{fontWeight:'bold', fontSize:20, marginBottom:10}}>It looks like a dancer!</Text>
+              <Text style={{fontWeight:'bold', fontSize:25, marginBottom:10, color:'#1058F4', textAlign:'center'}}>135th place</Text>
+              <TouchableOpacity style={styles.touchOpa}>
+                <Text style={{color:'white', fontWeight: 'bold', fontSize:12}}>Rechallenge?</Text>
+              </TouchableOpacity>
             </View>
           </View>
-          <View style={{backgroundColor: '#E9E8E8', width:390, height:4, marginTop:20}}></View>
-          {/* <View style={{margin:20}}>
-            <Text style={{fontWeight:'bold', fontSize:15}}>Wrong Result</Text>
-            <View style={{marginTop:30, backgroundColor: '#C4C4C4', width:350, height:150}}></View>
-          </View> */}
-        </ScrollView>
-      </SafeAreaView>
-    );
-  }
-
-
+        </View>
+        <View style={{backgroundColor: '#E9E8E8', width:390, height:4, marginTop:20}}></View>
+        {/* <View style={{margin:20}}>
+          <Text style={{fontWeight:'bold', fontSize:15}}>Wrong Result</Text>
+          <View style={{marginTop:30, backgroundColor: '#C4C4C4', width:350, height:150}}></View>
+        </View> */}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
